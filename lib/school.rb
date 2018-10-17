0d945ee47479dca305bef87763698db5254eaedc
Anuj Panchal <anujpan1@gmail.com>
2018-10-17 19:01:48 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,32 @@
+require 'pry'
+
 class School
   attr_accessor :roster
 
   def initialize(school_name)
+    @school_name = school_name
     @roster = Hash.new
   end
   
   def add_student(student, grade)
+    # if @roster.keys.include?(grade)
+    #   @roster[grade].push(student)
+    # else
+    #   @roster[grade] = [student]
+    # end
+    
+
+    # @roster[grade] = [] unless @roaster[grade]
+    
+
+    # @roaster[grade] || @roaster[grade] = []
+    
+
+    # @roster[grade] ? @roster[grade].push(student) : @roster[grade] = [student]
+    
     @roster[grade] ||= []
+
+
     @roster[grade].push(student)
   end
   
